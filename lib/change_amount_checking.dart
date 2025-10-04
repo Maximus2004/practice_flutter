59d92a57e11fbaff3a4000eb9a04c388d43a5923
madmitriev <madmitriev@rutube.ru>
2025-10-04 07:10:47 +0300

    Добавление экрана для измениения суммы на расчетном счете

---
+++ lib/change_amount_checking.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class ModifyAmountCheckingScreen extends StatefulWidget {
+  final int initialAmount;
+  final int step;
+  final ValueChanged<int> onAmountChanged;
+
+  const ModifyAmountCheckingScreen({
+    super.key,
+    required this.initialAmount,
+    required this.step,
+    required this.onAmountChanged,
+  });
+
+  @override
+  State<ModifyAmountCheckingScreen> createState() => _ModifyAmountCheckingScreen();
+}
+
+class _ModifyAmountCheckingScreen extends State<ModifyAmountCheckingScreen> {
+  late int amount;
+
+  @override
+  void initState() {
+    super.initState();
+    amount = widget.initialAmount;
+  }
+
+  void increase() {
+    setState(() {
+      amount += widget.step;
+    });
+  }
+
+  void decrease() {
+    setState(() {
+      amount -= widget.step;
+    });
+  }
+
+  @override
+  void dispose() {
+    widget.onAmountChanged(amount);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Сумма на вкладе"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('Текущая сумма: ${formatRub(amount)}',
+                style: const TextStyle(fontSize: 20)),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: increase,
+              child: const Text('Увеличить сумму'),
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: decrease,
+              child: const Text('Уменьшить сумму'),
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Готово'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+String formatRub(int amount) {
+  return '$amount р.';
+}
